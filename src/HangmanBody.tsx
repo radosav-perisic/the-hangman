4b6f4daa4a694b0cc43dae4cefe9da5e8e475cda
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-14 11:59:35 +0100

    Implement Hangman Body Component

--- src/HangmanBody.tsx
+++ src/HangmanBody.tsx
@@ -1,3 +1,14 @@
 export function HangmanBody() {
-
+  return (
+    <div style={{ position: "relative" }}>
+      <div
+        style={{
+          height: "400px",
+          width: "10px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      />
+    </div>
+  );
 }
