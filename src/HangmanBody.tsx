6685d37547c07b4e4b81f5b58062de355c3aa1bc
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-14 12:45:54 +0100

    Add Hangman Body

--- src/HangmanBody.tsx
+++ src/HangmanBody.tsx
@@ -1,19 +1,35 @@
 const HANGMAN_HEAD = (
-<div style={{
-    width:'50px',
-    height:'50px',
-    borderRadius: '100%',
-    border:'10px solid black',
-    position:'absolute',
-    top:'50px',
-    right:'-30px'
-}} />
-)
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
+);
+
+const HANGMAN_BODY = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      background: "black",
+      position: "absolute",
+      top: "120px",
+      right: 0,
+    }}
+  />
+);
 
 export function HangmanBody() {
   return (
     <div style={{ position: "relative" }}>
-        {HANGMAN_HEAD}
+      {HANGMAN_HEAD}
+      {HANGMAN_BODY}
       <div
         style={{
           position: "absolute",
