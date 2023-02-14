d1d73a97293b101812e964c5e79b3549c2efb9cf
Radosav Perisic <radosavperisic00@gmail.com>
2023-02-14 13:09:36 +0100

    Styled Word Component

--- src/Word.tsx
+++ src/Word.tsx
@@ -1,3 +1,15 @@
 export function Word() {
-    
+    const word  = 'test'
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >{word}</div>
+  );
 }
